561b071cf21e3e937007d3c78c9500ff217d5c61
Tomek-dev <siminiak.tomek@gmail.com>
2020-03-27 02:36:09 +0100

    Cors

--- src/main/java/com/app/barber/BarberApplication.java
+++ src/main/java/com/app/barber/BarberApplication.java
@@ -2,12 +2,29 @@ package com.app.barber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class BarberApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BarberApplication.class, args);
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 	//TODO email and user annotation, logout and ResponseEntity
